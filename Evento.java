e4a66fee780e377ad32bfdc4381eb90fbf0e9a62
JohauDEV <mlopki9000@gmail.com>
2025-11-24 17:31:48 -0300

    Enhance Evento class with VIP reservation feature
    Added functionality for VIP reservations and a list of events.

--- Evento.java
+++ Evento.java
@@ -5,11 +5,15 @@ public class Evento {
     private String nome;
     private int vagas;
     private List<Associado> participantes;
+    private List<Evento> eventos;
+
 
     public Evento(String nome, int vagas) {
         this.nome = nome;
         this.vagas = vagas;
         this.participantes = new ArrayList<>();
+        eventos = new ArrayList<>();
+
     }
 
     public boolean reservarVaga(Associado associado) {
@@ -24,4 +28,17 @@ public class Evento {
     public String toString() {
         return nome + " | Vagas restantes: " + (vagas - participantes.size());
     }
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean reservaVIP(Associado associado) {
+        // VIP sempre tenta entrar antes caso esteja no limite
+        if (participantes.size() < vagas) {
+            participantes.add(0, associado);  // VIP entra no topo
+            return true;
+        }
+        return false;
+    }
+
 }
