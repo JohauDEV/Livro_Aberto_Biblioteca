fb27d8ba799afbd6b985fe999c796f86868a671c
João Ricardo de Brito Melo Santos <mlopki9000@gmail.com>
2025-12-05 21:13:43 -0300

    Update Associado.java

--- Associado.java
+++ Associado.java
@@ -3,15 +3,15 @@ import java.util.List;
 
 public class Associado {
     protected String nome;
-    protected String tipo;
-    protected int prazoEmprestimo = 10;
-    protected double descontoMulta = 0.0;
+    protected String tipo; // "Regular" ou "VIP"
+    protected int prazoEmprestimo;
+    protected double descontoMulta;
     protected List<Emprestimo> historico;
 
     public Associado(String nome) {
         this.nome = nome;
         this.tipo = "Regular";
-        this.prazoEmprestimo = 7; // dias
+        this.prazoEmprestimo = 10; // padrão Regular
         this.descontoMulta = 0.0;
         this.historico = new ArrayList<>();
     }
@@ -20,6 +20,7 @@ public class Associado {
     public String getTipo() { return tipo; }
     public int getPrazoEmprestimo() { return prazoEmprestimo; }
     public double getDescontoMulta() { return descontoMulta; }
+    public List<Emprestimo> getHistorico() { return historico; }
 
     public void adicionarEmprestimo(Emprestimo e) { historico.add(e); }
 
